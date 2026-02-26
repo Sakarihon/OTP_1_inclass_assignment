cfc37991832a09bb2672d2052b1f713622498955
sakari Honkavaara <Sakari.honkavaara@metropolia.fi>
2026-02-26 13:42:22 +0200

    .

--- OTP1_inclass_assignment/src/test/java/TemperatureConverterTest.java
+++ OTP1_inclass_assignment/src/test/java/TemperatureConverterTest.java
@@ -3,27 +3,32 @@ import org.junit.jupiter.api.Test;
 
 class TemperatureConverterTest {
 
-    TemperatureConverter converter = new TemperatureConverter();
-
     @Test
     void fahrenheitToCelsius() {
-        assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.001);
-        assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.001);
-        assertEquals(-40.0, converter.fahrenheitToCelsius(-40), 0.001);
+        assertEquals(0.0, TemperatureConverter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100.0, TemperatureConverter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(-40.0, TemperatureConverter.fahrenheitToCelsius(-40), 0.001);
     }
-,
+
     @Test
     void celsiusToFahrenheit() {
-        assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.001);
-        assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.001);
-        assertEquals(-40.0, converter.celsiusToFahrenheit(-40), 0.001);
+        assertEquals(32.0, TemperatureConverter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212.0, TemperatureConverter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(-40.0, TemperatureConverter.celsiusToFahrenheit(-40), 0.001);
     }
 
     @Test
     void isExtremeTemperature() {
-        assertTrue(converter.isExtremeTemperature(-50));
-        assertTrue(converter.isExtremeTemperature(60));
-        assertFalse(converter.isExtremeTemperature(0));
-        assertFalse(converter.isExtremeTemperature(25));
+        assertTrue(TemperatureConverter.isExtremeTemperature(-50));
+        assertTrue(TemperatureConverter.isExtremeTemperature(60));
+        assertFalse(TemperatureConverter.isExtremeTemperature(0));
+        assertFalse(TemperatureConverter.isExtremeTemperature(25));
+    }
+
+    @Test
+    void kelvinToCelsius() {
+        assertEquals(26.85, TemperatureConverter.kelvinToCelsius(300), 0.001);
+        assertEquals(0.0, TemperatureConverter.kelvinToCelsius(273.15), 0.001);
+        assertEquals(-273.15, TemperatureConverter.kelvinToCelsius(0), 0.001);
     }
 }
